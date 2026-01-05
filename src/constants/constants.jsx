142977191409c9ffbb684672a76ec9f27f49de5d
pachara <pachared.amr12@gmail.com>
2026-01-05 18:49:51 +0700

    ไฟล์ข้อมูลต่างๆของ Portfoilo

---
+++ src/constants/constants.jsx
@@ -0,0 +1,14 @@
+export const menuItems = [
+    { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
+    { label: 'Hero', ariaLabel: 'Learn about us', link: '/Hero' },
+    { label: 'Projects', ariaLabel: 'View our services', link: '/Projects' },
+    { label: 'Skills', ariaLabel: 'Get in touch', link: '/Skills' },
+    { label: 'Education', ariaLabel: 'Get in touch', link: '/Education' },
+    { label: 'Contact', ariaLabel: 'Get in touch', link: '/Contact' }
+];
+
+export const socialItems = [
+    { label: 'Facebook', link: 'https://facebook.com' },
+    { label: 'GitHub', link: 'https://github.com' },
+    { label: 'Instagram', link: 'https://instagram.com' }
+];
