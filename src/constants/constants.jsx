42cf1f7290d46a99b6c23731caea2336d01214cc
pachara <pachared.amr12@gmail.com>
2026-01-07 23:40:44 +0700

    เพิ่มข้อมูลของ Hero กับ About

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -12,3 +12,18 @@ export const socialItems = [
     { label: 'GitHub', link: 'https://github.com' },
     { label: 'Instagram', link: 'https://instagram.com' }
 ];
+
+export const HERO = {
+    name: "PACHARA",
+    greet: "Front-End Developer",
+    greet2: "Back-End Developer",
+    greet3: "Full-Stack Developer",
+    greet4: "Lead Front-End Developer",
+    description:
+        "ฉันเป็น Front-End Developer ที่หลงใหลในการออกแบบและสร้างสรรค์ UI ที่สวยงามและตอบโจทย์ประสบการณ์ผู้ใช้งาน พร้อมมุ่งมั่นพัฒนาทักษะด้านการเขียนโค้ดและแก้ปัญหาอย่างเป็นระบบ เพื่อเติบโตสู่การเป็น Lead Front-End Developer ในอนาคต และเปิดรับความท้าทายใหม่ ๆ อยู่เสมอ",
+};
+
+export const ABOUT = [
+    "ผมเป็นนักศึกษาระดับปริญญาตรี จากมหาวิทยาลัยเทคโนโลยีราชมงคลสุวรรณภูมิ ศูนย์สุพรรณบุรี มีความหลงใหลในการออกแบบและพัฒนาเว็บไซต์ โดยเฉพาะในสายงาน Front-End Developer พร้อมให้ความสำคัญกับการสร้างสรรค์ผลงานที่สวยงามและตอบโจทย์ผู้ใช้งานอยู่เสมอ",
+    "ด้วยความมุ่งมั่นในการเรียนรู้และพัฒนาทักษะอย่างต่อเนื่อง ผมตั้งเป้าหมายในการเติบโตสู่การเป็น Lead Front-End Developer โดยเน้นการออกแบบซอฟต์แวร์ที่มีคุณภาพ และประสบการณ์ผู้ใช้ที่ดีเยี่ยมควบคู่กันไป",
+];
