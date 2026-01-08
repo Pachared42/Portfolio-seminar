f45bf0cb0b1f26be3d0de42e3a3d93af3b1ec9f7
pachara <pachared.amr12@gmail.com>
2026-01-09 02:48:32 +0700

    เพิ่ม export const NAVTGATION_LINKS

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,11 +1,10 @@
-export const menuItems = [
-    { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
-    { label: 'Hero', ariaLabel: 'Learn about us', link: '/Hero' },
-    { label: 'Projects', ariaLabel: 'View our services', link: '/Projects' },
-    { label: 'Skills', ariaLabel: 'Get in touch', link: '/Skills' },
-    { label: 'Education', ariaLabel: 'Get in touch', link: '/Education' },
-    { label: 'Contact', ariaLabel: 'Get in touch', link: '/Contact' }
-];
+export const NAVTGATION_LINKS = [
+    { label: "ประวัติ", href: "#about"},
+    { label: "ผลงาน", href: "#projects"},
+    { label: "การศึกษา", href: "#education"},
+    { label: "ทักษะ", href: "#skills"},
+    { label: "ติดต่อ", href: "#contact"},
+]
 
 export const socialItems = [
     { label: 'Facebook', link: 'https://facebook.com' },
