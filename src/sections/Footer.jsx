8df11967da55d94efd081ab3322ce316118a1ba1
pachara <pachared.amr12@gmail.com>
2026-01-18 00:52:02 +0700

    เปลี่ยน code เล็กน้อยในส่วนแสดงปีให้เป็นปีปัจจุบัน

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Footer() {
     return (
         <section>
-            <div className='pt-80'>
+            <div className='pt-80 pb-10'>
                 <div className=''>
-                    <p className='text-white text-center text-[1rem]'>© 2025 Pachara Portfolio. All rights reserved.</p>
+                    <p className='text-white text-center text-[1rem]'>© {new Date().getFullYear()} Seminar Portfolio. All rights reserved.</p>
                 </div>
             </div>
         </section>
