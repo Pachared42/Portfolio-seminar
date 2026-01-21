33aa4bbb8757127f66d78085f15c5a103460acba
Axm.pachara <pachared.amr12@gmail.com>
2026-01-21 17:11:41 +0700

    ปรับแต่งการแสดง Text ABOUT ME ให้เหมาะสมกับ Mobile

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -80,11 +80,7 @@ function About() {
 
         <motion.h1
           variants={fadeUp}
-          className="text-[3.5rem] sm:text-[4.5rem] md:text-[6rem] lg:text-[9rem] lx:text-[11rem] font-bold text-center"
-          style={{
-            WebkitTextStroke: "4px black",
-            color: "transparent",
-          }}
+          className="text-[3.5rem] sm:text-[4.5rem] md:text-[6rem] lg:text-[9rem] lx:text-[11rem] font-bold text-center text-transparent [-webkit-text-stroke:1px_black] md:[-webkit-text-stroke:0.5px_black] sm:[-webkit-text-stroke:0.5px_black]"
         >
           ABOUT ME
         </motion.h1>
