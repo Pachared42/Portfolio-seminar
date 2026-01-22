114bac4e5aced3f588c78f9d88eb27dfe356821f
pachara <pachared.amr12@gmail.com>
2026-01-22 23:30:27 +0700

    ปรับแต่งแบบ responsive ของไฟล์ Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
     return (
-        <section>
-            <div className='pt-80 pb-10'>
-                <div className=''>
-                    <p className='text-white text-center text-[1rem]'>© {new Date().getFullYear()} Seminar Portfolio. All rights reserved.</p>
-                </div>
-            </div>
-        </section>
-    )
+        <footer className="pt-32 sm:pt-40 lg:pt-56 pb-8">
+            <p className="text-center text-sm sm:text-base text-white/80">
+                © {new Date().getFullYear()} Seminar Portfolio. All rights reserved.
+            </p>
+        </footer>
+    );
 }
 
-export default Footer
+export default Footer;
