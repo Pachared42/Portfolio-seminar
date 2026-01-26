a545659015aa222c846efa28582a62eed7e20a54
pachara <pachared.amr12@gmail.com>
2026-01-26 18:19:51 +0700

    ปรับเปลี่ยน grid ให้เหมาะสมกับขนาดหน้าจอต่างๆ

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -43,11 +43,11 @@ function Skills() {
       variants={gridContainer}
       className="
         grid
-        grid-cols-2
-        sm:grid-cols-3
-        md:grid-cols-4
-        lg:grid-cols-6
-        gap-x-8 gap-y-12
+        grid-cols-4
+        sm:grid-cols-5
+        md:grid-cols-6
+        lg:grid-cols-7
+        gap-y-10
         place-items-center
       "
     >
