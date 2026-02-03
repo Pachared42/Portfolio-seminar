de9351ab389b111b827e2c7b8a2fcce84bffedb3
Axm.pachara <pachared.amr12@gmail.com>
2026-02-04 00:07:06 +0700

    ปรับเปลี่ยน footer ใหม่ให้สวยขี้น

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
 function Footer() {
-    return (
-        <footer className="pt-32 sm:pt-40 lg:pt-56 pb-8">
-            <p className="text-center text-sm sm:text-base text-white/80">
-                © {new Date().getFullYear()} Seminar Portfolio. All rights reserved.
-            </p>
-        </footer>
-    );
+  return (
+    <footer className="text-center pt-32 sm:pt-40 lg:pt-56 pb-8">
+      <p className="text-sm sm:text-base text-slate-400">
+        © {new Date().getFullYear()}{" "}
+        <span className="font-medium text-slate-300">
+          Frontend Devfolio
+        </span>
+      </p>
+
+      <p className="mt-1 text-xs sm:text-sm text-slate-500">
+        Built by{" "}
+        <span className="text-slate-300 font-medium">Pachara Kalapakdee</span>
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
