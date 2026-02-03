cf6e71c65c8c82b0259847072ec27b1728fa05a9
Axm.pachara <pachared.amr12@gmail.com>
2026-02-04 00:07:51 +0700

    ปรับ motion ใหม่และก็เพิ่ม fadeUpSlow

--- src/sections/ImageShowcase.jsx
+++ src/sections/ImageShowcase.jsx
@@ -1,56 +1,69 @@
-import React from 'react'
-import CircularGallery from '../components/CircularGallery'
-import { IMAGE_SHOWCASE } from '../constants/constants';
-import { motion } from 'motion/react';
+import React from "react";
+import CircularGallery from "../components/CircularGallery";
+import { IMAGE_SHOWCASE } from "../constants/constants";
+import { motion } from "motion/react";
 
 function ImageShowcase() {
+  const container = {
+    hidden: {},
+    show: {
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.15,
+      },
+    },
+  };
 
-    const container = {
-        hidden: {},
-        show: {
-            transition: {
-                delayChildren: 0.2,
-                staggerChildren: 0.15,
-            },
-        },
-    };
+  const fadeUp = {
+    hidden: { opacity: 0, y: 40 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5, ease: [0.22, 1, 0.36, 1] },
+    },
+  };
 
-    const fadeUp = {
-        hidden: { opacity: 0, y: 40 },
-        show: {
-            opacity: 1,
-            y: 0,
-            transition: { duration: 0.5, ease: [0.22, 1, 0.36, 1] }
-        }
-    }
-    return (
-        <section>
-            <motion.div
-                variants={container}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: true, amount: 0.4 }}
-                className='relative h-[30vh] md:h-[50vh] lg:h-[70vh]'
-            >
-                <h2
-                    variants={fadeUp}
-                    className="
+  const fadeUpSlow = {
+    hidden: { opacity: 0, y: 60 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.9, delay: 0.2 },
+    },
+  };
+
+  return (
+    <section>
+      <motion.div
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.4 }}
+      >
+        <motion.h2
+          variants={fadeUp}
+          className="
             text-center font-bold text-white
             text-[clamp(1.75rem,3vw,2.25rem)]
           "
-                >
-                    {IMAGE_SHOWCASE.title}
-                </h2>
-                <CircularGallery
-                    bend={0.3}
-                    textColor="#ffffff"
-                    borderRadius={0.05}
-                    scrollEase={0.02}
-                    scrollSpeed={1}
-                />
-            </motion.div>
-        </section>
-    )
+        >
+          {IMAGE_SHOWCASE.title}
+        </motion.h2>
+        <motion.div
+          className="relative h-[25vh] md:h-[41vh] lg:h-[50vh]"
+          variants={fadeUpSlow}
+        >
+          <CircularGallery
+            bend={0.3}
+            textColor="#ffffff"
+            borderRadius={0.05}
+            scrollEase={0.02}
+            scrollSpeed={1}
+          />
+        </motion.div>
+      </motion.div>
+    </section>
+  );
 }
 
-export default ImageShowcase
+export default ImageShowcase;
