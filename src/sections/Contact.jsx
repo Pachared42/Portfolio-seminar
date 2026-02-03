8ae8eb0cddeb923ff43b5edda52c94d7a17f3838
Axm.pachara <pachared.amr12@gmail.com>
2026-02-04 00:06:43 +0700

    เอา import delay ของ framer-motion ออก

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SOCIAL_MEDIA_LINKS } from "../constants/constants";
-import { delay, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 function Contact() {
     const sectionContainer = {
